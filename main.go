785701e06c7bf2f83da2d288d3c950df069563df
Mark Colby <macolby14@gmail.com>
2022-04-16 08:14:45 -0400

    remove normal routes

--- main.go
+++ main.go
@@ -13,26 +13,9 @@ func home(w http.ResponseWriter, req *http.Request) {
 }
 
 
-func hello(w http.ResponseWriter, req *http.Request){
-	fmt.Fprintf(w, "hello\n")
-}
-
-func headers(w http.ResponseWriter, req *http.Request){
-	for name, headers := range req.Header {
-		for _, h:= range headers {
-			fmt.Fprintf(w, "%v : %v\n", name, h)
-		}
-	}
-}
-
 func main() {
 	router := pat.New()
-	
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/headers", headers)
-
 	router.Get("/", home)
-
 	http.Handle("/", router)
 
 	fmt.Println("Starting webserver...")
