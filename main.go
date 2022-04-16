6b47170f1873336e2f6d5aeecc53ca1fed78e773
Mark Colby <macolby14@gmail.com>
2022-04-16 13:54:54 -0400

    write randome session cookie on startup

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/gorilla/pat"
+	"github.com/gorilla/securecookie"
 	"github.com/joho/godotenv"
 	"github.com/markbates/goth"
 	"github.com/markbates/goth/gothic"
@@ -15,10 +16,26 @@ import (
 
 func auth(res http.ResponseWriter, req *http.Request) {
 	if user, err := gothic.CompleteUserAuth(res, req); err == nil {
-		fmt.Fprintf(res, "Auth a success %v %v", res, user)
+		fmt.Fprintf(res, "Auth already complete %v %v", res, user)
 	} else {
+		fmt.Println("Beginning auth")
 		gothic.BeginAuthHandler(res, req)
 	}
+}
+
+func authCallback(res http.ResponseWriter, req *http.Request) {
+	user, err := gothic.CompleteUserAuth(res, req)
+	if err != nil {
+		fmt.Fprintln(res, err)
+		return
+	}
+	fmt.Fprintf(res, "User info %v", user)
+}
+
+func logout(res http.ResponseWriter, req *http.Request) {
+	gothic.Logout(res, req)
+	res.Header().Set("Location", "/")
+	res.WriteHeader(http.StatusTemporaryRedirect)
 }
 
 func home(res http.ResponseWriter, req *http.Request) {
@@ -31,9 +48,12 @@ func main() {
 		log.Fatal("Errir loading .env file")
 	}
 
+	os.Setenv("SESSION_SECRET", string(securecookie.GenerateRandomKey(32)))
+
 	goth.UseProviders(google.New(os.Getenv("GOOGLE_OAUTH_CLIENT_ID"), os.Getenv("GOOGLE_OAUTH_SECRET"), "http://localhost:8080/auth/google/callback"))
 
 	router := pat.New()
+	router.Get("/auth/{provider}/callback", authCallback)
 	router.Get("/auth/{provider}", auth)
 	router.Get("/", home)
 	http.Handle("/", router)
