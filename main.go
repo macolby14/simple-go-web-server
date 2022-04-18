20318758fa1ea8519230a6f4d200154427a5aef6
Mark Colby <macolby14@gmail.com>
2022-04-17 21:00:49 -0400

    add subrouter for protected routes

--- main.go
+++ main.go
@@ -15,6 +15,23 @@ func health(res http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(res).Encode(map[string]bool{"ok": true})
 }
 
+func authMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		session, err := store.Get(req, "app-session")
+		if err != nil {
+			log.Printf("[ERROR] getting a session: %v\n", err)
+			return
+		}
+		_, ok := session.Values["user"]
+		if ok {
+			next.ServeHTTP(res, req)
+		} else {
+			res.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(res).Encode(map[string]interface{}{"ok": false, "error": "User session not found by middleware for protected route"})
+		}
+	})
+}
+
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
@@ -23,11 +40,14 @@ func main() {
 	authInit()
 
 	router := mux.NewRouter()
+	protectedRotuer := router.PathPrefix("/api/protected").Subrouter()
+	protectedRotuer.Use(authMiddleware)
 	router.HandleFunc("/api/auth/user", authUser)
 	router.HandleFunc("/api/auth/{provider}/callback", authCallback)
 	router.HandleFunc("/api/auth/{provider}/logout", authLogout)
 	router.HandleFunc("/api/auth/{provider}", auth)
 	router.HandleFunc("/api/health", health)
+	protectedRotuer.HandleFunc("/health", health)
 	http.Handle("/", router)
 
 	log.Println("Starting webserver...")
