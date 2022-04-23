35e324be032c38af3fa6c5f5603be3b3f4477464
Mark Colby <macolby14@gmail.com>
2022-04-23 15:35:16 -0400

    test insert into db

--- user.go
+++ user.go
@@ -3,6 +3,8 @@ package main
 import (
 	"database/sql"
 	"log"
+
+	_ "modernc.org/sqlite"
 )
 
 type User struct {
@@ -14,12 +16,40 @@ type User struct {
 var db *sql.DB
 
 func dbInit() {
-	dsnUri := "sqlite:./db/main.db"
+	dsnUri := "/home/mark/simple-go-web-server/db/main.db"
 	var err error
 	db, err = sql.Open("sqlite", dsnUri)
 	if err != nil {
 		log.Fatalf("[ERROR] connection to db failed. %v\n", err)
 	}
+
+	log.Println(db)
+
+	testInsertQ := `
+	    INSERT INTO user (name, email, avatarUrl, timeCreated)
+	    VALUES ('test', 'test', 'test', 100);
+		SELECT * FROM user;
+	`
+	rows, err := db.Query(testInsertQ)
+	if err != nil {
+		log.Printf("[ERROR] Error with db query. %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var (
+			id          int
+			name        string
+			email       string
+			avatarUrl   string
+			timeCreated int
+		)
+		if err := rows.Scan(&id, &name, &email, &avatarUrl, &timeCreated); err != nil {
+			log.Printf("[ERROR] Error scanning row. %v\n", err)
+		}
+		log.Printf("Results: %v %v %v %v %v\n", id, name, email, avatarUrl, timeCreated)
+	}
+
 }
 
 // func getOrCreateUser(user goth.User) User {
