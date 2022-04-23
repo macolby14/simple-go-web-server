826d522f0d401eda91048888eb4fb03beba8b644
Mark Colby <macolby14@gmail.com>
2022-04-23 15:39:00 -0400

    make email unique for user table

--- db/database.sh
+++ db/database.sh
@@ -3,5 +3,5 @@
 echo "Starting db init"
 
 sqlite3 main.db <<EOF
-CREATE TABLE user (id INTEGER PRIMARY KEY, name TEXT, email TEXT, avatarUrl TEXT, timeCreated INTEGER);
+CREATE TABLE user (id INTEGER PRIMARY KEY, name TEXT, email TEXT UNIQUE, avatarUrl TEXT, timeCreated INTEGER);
 EOF
